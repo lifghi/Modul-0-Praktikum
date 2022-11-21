769ee6b7d6626ca63f094b256eab5d6429578028
lifghi <115357938+lifghi@users.noreply.github.com>
2022-11-21 23:28:36 +0800

    Add files via upload

---
+++ Modul7/soal1/PRAK601-2210817310014-FatihAlifGhifari.py
@@ -0,0 +1,11 @@
+a,b = map(int,(input().split()))
+matrix = []
+x = 0
+isi = list(map(int, input().split()))
+for i in range(a):          
+    matrix.append(isi[x : x + b])
+    x = x + b
+for a in matrix:
+    for isi in a:
+        print(isi, end = " ")
+    print( )
