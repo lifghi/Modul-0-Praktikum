d856ddf8c6bd33657fda72c42cf49468c43f9e7c
lifghi <115357938+lifghi@users.noreply.github.com>
2022-11-26 03:10:01 +0800

    Delete BAHASA PY directory

--- BAHASA PY/PRAK002-2210817310014-Fatih Alif Ghifari.py.py
+++
@@ -1,3 +0,0 @@
-print("Selamat pagi, Fatih Alif Ghifari")
-print("Selamat siang, Fatih Alif Ghifari")
-print("Selamat malam, Fatih Alif Ghifari")
--- BAHASA PY/PRAK004-2210817310014-Fatih Alif Ghifari.py.py
+++
@@ -1,6 +0,0 @@
-print("#"*40)
-print("\n\n#\t\t\t                           #\n");
-print("\n#          Fatih Alif Ghifari          #\n");
-print("\n#            2210817310014             #\n");
-print("\n#\t\t\t                           #\n\n");
-print("#"*40)   
