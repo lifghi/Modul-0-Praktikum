effe51a825c4cb5c2c7ba95a712368c01ea1d796
lifghi <115357938+lifghi@users.noreply.github.com>
2022-11-26 03:10:32 +0800

    Delete Modul 3 directory

--- Modul 3/Soal 2/PRAK202-2210817310014-Fatih Alif Ghifari.py
+++
@@ -1,4 +0,0 @@
-n1 = float(input("Masukan Nilai Pertaman : "))
-n2 = float(input("Masukan Nilai Kedua    : "))
-r  = n1+n2
-print("Hasil dari penjumlahan nilai pertama \"%.2f\" dan nilai kedua \"%.2f\" adalah \"%.2f\"" %(n1, n2, r))
